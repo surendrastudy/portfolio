b45acb032a3a6517dd1ecd1c5d16a0291915c063
krishna mahto <mahtok98212@gmail.com>
2023-11-16 14:06:49 +0530

    Add about the projects to the webApp

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,56 +15,82 @@ const Portfolio = () => {
   const portfolioItems = [
     {
       src: chatApp,
+      name: "chatApp",
+      about: "This is chat App made upon socket.io with MERN stack",
       codeLink: "https://github.com/krishnamahto021/chatApp",
       demoLink: "https://chatapp-inyr.onrender.com/",
     },
     {
       src: calculator,
+      name: "Calculator",
+      about: "This is Calculator made upon the react js",
       codeLink: "https://github.com/krishnamahto021/calculator",
       demoLink: "https://krishnamahto021.github.io/calculator/",
     },
     {
       src: albumCreator,
+      name: "Album Creator",
+      about:
+        "This is Album webapp that helps to create your albums to store your memories",
       codeLink: "https://github.com/krishnamahto021/albumCreator",
       demoLink: "https://github.com/krishnamahto021/albumCreator",
     },
     {
       src: codingNinjasPage,
-      codeLink: "https://github.com/krishnamahto021/chatApp",
-      demoLink: "https://chatapp-inyr.onrender.com/",
+      name: "Course Page Clone",
+      about: "This is coding Ninjas course page Clone made using react js ",
+      codeLink: "https://github.com/krishnamahto021/codingNinjasCourses",
+      demoLink: "https://github.com/krishnamahto021/codingNinjasCourses",
     },
     {
       src: ecommerce,
+      name: "BusyBuy",
+      about:
+        "This is ecommerce  made on the react js with firebase as database",
       codeLink: "https://github.com/krishnamahto021/busyBuy",
       demoLink: "https://github.com/krishnamahto021/busyBuy",
     },
     {
       src: expenseTracker,
+      name: "Expense Tracker",
+      about: "This is expense tracker made to track all your expenses",
       codeLink: "https://github.com/krishnamahto021/expenseTracker",
       demoLink: "https://github.com/krishnamahto021/expenseTracker",
     },
     {
       src: habitTracker,
+      name: "Habit Tracker",
+      about: "This is webApp made for the users who want to track their habits",
       codeLink: "https://github.com/krishnamahto021/habit_tracker",
       demoLink: "https://github.com/krishnamahto021/habit_tracker",
     },
     {
       src: ogStore,
+      name: "ogStore",
+      about:
+        "This is admin dashboard of the ogstore from where the admin can add the product ",
       codeLink: "https://github.com/krishnamahto021/ecommerce-frontend",
       demoLink: "https://github.com/krishnamahto021/ecommerce-frontend",
     },
     {
       src: ipod,
+      name: "iPod",
+      about: "This is iPod clone made on the reactJs",
       codeLink: "https://github.com/krishnamahto021/iPod",
       demoLink: "https://github.com/krishnamahto021/iPod",
     },
     {
       src: photoFolio,
+      name: "photoFolio",
+      about: "This is webApp to provide you a digital photos collection",
       codeLink: "https://github.com/krishnamahto021/photoFolio",
       demoLink: "https://github.com/krishnamahto021/photoFolio",
     },
     {
       src: fbClone,
+      name: "Facebook authentication Clone",
+      about:
+        "This is clone for facebook sign up sign in page authorizing the user through session cookie and gooogle oAuth2",
       codeLink: "https://github.com/krishnamahto021/fb_login_clone",
       demoLink: "https://github.com/krishnamahto021/fb_login_clone",
     },
@@ -92,6 +118,14 @@ const Portfolio = () => {
                 alt={`project-${index}`}
                 className="rounded-lg w-full h-64 p-4 object-cover duration-200 scale-105"
               />
+              <div className="flex flex-col gap-2 max-h-28 ml-2 p-2">
+                <p className="capitalize font-extrabold text-gray-400">
+                  {p.name}
+                </p>
+                <p className="capitalize font-medium text-gray-200">
+                  {p.about}
+                </p>
+              </div>
               <div className="buttonsContainer flex items-center ml-2 justify-center">
                 <a
                   href={p.demoLink}
--- src/socialLinks.js
+++ src/socialLinks.js
@@ -7,7 +7,13 @@ const SocialLinks = () => {
   return (
     <div className="socialContainer flex-col items-center fixed top-[30%] left-0 hidden lg:block">
       <div className="cursor-pointer flex justify-between -ml-[100px] px-4 bg-gray-500 w-36 h-14 items-center text-white text-xl hover:rounded-md hover:-ml-[10px] duration-300">
-        GitHub
+        <a
+          href="https://github.com/krishnamahto021"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
         <a
           href="https://github.com/krishnamahto021"
           target="_blank"
@@ -17,7 +23,13 @@ const SocialLinks = () => {
         </a>
       </div>
       <div className="cursor-pointer flex justify-between -ml-[100px] px-4 bg-gray-500 w-36 h-14 items-center text-white text-xl hover:rounded-md hover:-ml-[10px] duration-300">
-        LinkedIn
+        <a
+          href="https://www.linkedin.com/in/krishnamahto021/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Linkedin
+        </a>
         <a
           href="https://www.linkedin.com/in/krishnamahto021/"
           target="_blank"
@@ -27,13 +39,22 @@ const SocialLinks = () => {
         </a>
       </div>
       <div className="cursor-pointer flex justify-between -ml-[100px] px-4 bg-gray-500 w-36 h-14 items-center text-white text-xl hover:rounded-md hover:-ml-[10px] duration-300">
-        Mail
+        <a href="mailto:mahtok98212@gmail.com" target="_blank" rel="noreferrer">
+          Mail
+        </a>
         <a href="mailto:mahtok98212@gmail.com" target="_blank" rel="noreferrer">
           <HiOutlineMail />
         </a>
       </div>
       <div className="cursor-pointer flex justify-between -ml-[100px] px-4 bg-gray-500 w-36 h-14 items-center text-white text-xl hover:rounded-md hover:-ml-[10px] duration-300">
-        Resume
+        <a
+          href="/krishnaResume.pdf"
+          target="_blank"
+          rel="noreferrer"
+          download={true}
+        >
+          Resume
+        </a>
         <a
           href="/krishnaResume.pdf"
           target="_blank"
