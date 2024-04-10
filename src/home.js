76fe57a28fbf84675408644572789c7a8d781893
krishna mahto <mahtok98212@gmail.com>
2024-04-10 22:11:50 +0530

    add the modified resume

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImage from "./assets/heroImage.png";
+import heroImage from "./assets/heroImage.jpg";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import ReactTypingEffect from "react-typing-effect";
 import { Link } from "react-scroll";
@@ -8,10 +8,10 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="homeContainer max-h-fit  w-full bg-gradient-to-b from-black to-gray-800 flex flex-col md:flex-row items-center justify-around p-4"
+      className="homeContainer max-h-fit bg-gradient-to-b from-black to-gray-800 flex flex-col md:flex-row items-center justify-around p-4 pt-20 "
     >
-      <div className="left w-full md:w-[60vw] mt-16 md:mt-0 md:flex md:flex-col items-center justify-center h-full">
-        <div className="aboutMyself text-3xl font-bold text-gray-300 flex flex-col content-center md:ml-14">
+      <div className="left mt-16 flex-col md:mt-0 md:flex  items-center justify-center h-full">
+        <div className="aboutMyself text-3xl font-bold text-gray-300 flex flex-col content-center md:ml-14 p-4">
           <ReactTypingEffect
             staticText="I am "
             text={[
@@ -20,7 +20,7 @@ const Home = () => {
               "Zero Experience Enthusiast",
             ]}
           />
-          <p className="text-lg">
+          <p className="text-2xl leading-10  mt-6  ">
             As a budding MERN stack developer, I bring a fresh perspective and
             various diverse projects showcasing my commitment to crafting
             user-centric, efficient web solutions. A quick learner with zero
@@ -43,12 +43,8 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className="right flex ">
-        <img
-          src={heroImage}
-          alt="Krishna"
-          className="rounded-3xl aspect-square"
-        />
+      <div className="right flex w-full md:w-10/12 ">
+        <img src={heroImage} alt="Krishna" className="rounded-xl md:rounded-full " />
       </div>
     </div>
   );
